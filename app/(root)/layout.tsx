7b93efde79d1e76a541a959d9012fadaa4ac8ccf
mountainsea-lab <w1999wtw3537@gmail.com>
2025-10-29 21:40:38 +0800

    add app **

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,28 +1,29 @@
-import Header from "@/components/Header";
-import { auth } from "@/lib/better-auth/auth";
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
 import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import { getSession } from "@/lib/actions/auth.actions";
+import { redirect } from "next/navigation";
 
-const Layout = async ({ children }: { children: React.ReactNode }) => {
-  const session = await auth.api.getSession({ headers: await headers() });
+const Layout = async ({ children }: { children : React.ReactNode }) => {
+    const session = await getSession();
 
-  if (!session?.user) redirect("/sign-in");
+    if(!session?.user) redirect('/sign-in');
 
-  const user = {
-    id: session.user.id,
-    name: session.user.name,
-    email: session.user.email,
-  };
+    const user = {
+        id: session.user.id,
+        name: session.user.name,
+        email: session.user.email,
+    }
 
-  return (
-    <main className="min-h-screen text-gray-400">
-      <Header user={user} />
+    return (
+        <main className="min-h-screen text-gray-400">
+            <Header user={user} />
 
-      <div className="container py-10">{children}</div>
+            <div className="container py-10">
+                {children}
+            </div>
 
-      <Footer />
-    </main>
-  );
-};
-export default Layout;
+            <Footer />
+        </main>
+    )
+}
+export default Layout
